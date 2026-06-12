c634ace321ca6fc9b2c1534282f65966b8b666cd
sneha <sneha_gadre@yahoo.com>
2026-06-12 11:01:38 -0700

    simplifying code

--- src/authorizer.py
+++ src/authorizer.py
@@ -1,13 +1,5 @@
 import json
 import logging
-import os
-import time
-from logging import Logger
-from typing import Dict, Any, List, Optional
-
-import boto3
-import jwt
-from jwt.jwks_client import PyJWKClient
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
